1629d31fc15bec46726c7831ddfad208479e5948
Alexandra Bouillon <Alexandra@the beans.io>
2022-01-30 07:52:37 -0800

    lift off 2 complete

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,7 +5,18 @@ const resolvers = {
         // tracksForHome: (parent, args, context, info) => {},
 
         // get all tracks, will be used to populate the homepage grid of our web client
-        tracksForHome: (_, __, {dataSources}) => {}
+        // args, parents, are not being used so we have underscores
+        // dataSources is being destructure from the context
+        // info is omitted, ALL params are optional
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
     }
+
 };
 module.exports = resolvers;
